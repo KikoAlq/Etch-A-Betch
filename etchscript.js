ead322d3f7544a900cf6b9f41494d95d9c37becd
Kiko Alquiros <kiksdoesstuff@gmail.com>
2023-06-05 11:20:27 -0400

    got back bby

--- etchscript.js
+++ etchscript.js
@@ -1,24 +1,18 @@
-const value = document.querySelector('#reset')
+const number = document.getElementById('reset')
 const bigBox = document.querySelector('#Box')
 const button = document.querySelector('#setNumber')
-let divStyles = {
-    'background-color': 'rgb(109, 103, 86)',
-    'color': 'grey',
-    'border': '1px',
-    'display': 'flex',
-    'flex-wrap': 'wrap',
-    'width': '25vh',
-    'height': '25vh',
-    'max-width': '25vh',
-    'max-height': '25vh'
-};
-for (let x=1; x<=16; x++) {
-    const div = document.createElement('div') 
-    Object.assign(div.style, divStyles)
+const trapse = document.querySelectorAll('.cum')
+
+function setBox() {
+for (let x=1; x<=number.value; x++) {
+    const div = document.createElement('div');
     bigBox.appendChild(div);
-    div.setAttribute('id', x);
+    div.className = "cum";
 };
-button.addEventListener("click", test)
-function test() {
-    console.log(value)
 }
+
+button.addEventListener("click", setBox)
+
+console.log(trapse)
+
+trapse.onmouseenter.style.backgroundColor = "black";
